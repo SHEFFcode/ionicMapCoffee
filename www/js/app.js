bd90df2116f8256c48025baaebf664c348c861e0
Yuriy Shefer <sheff@sheffmachine.com>
2016-05-29 02:51:56 -0700

    ionic coffee

---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('caffeinehit', [
+	'ionic',
+  'ngMap',
+  'ngCordova',
+	'caffeinehit.controllers',
+	'caffeinehit.services',
+	'caffeinehit.filters'
+]);
+
+app.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
+
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+});
+
+app.config(function ($httpProvider) {
+	$httpProvider.defaults.headers.common['Authorization'] = 'Token 66bbe2f7472855a23ca5d6b4726ded5f1b39fe64';
+});
